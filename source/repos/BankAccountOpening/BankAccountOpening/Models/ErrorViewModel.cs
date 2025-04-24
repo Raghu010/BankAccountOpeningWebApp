8b624ce1910d63102ba99ae106270b540fef959b
Raghu <raghuraghav929@gmail.com>
2025-04-24 18:55:08 +0530

    initial commit

---
+++ source/repos/BankAccountOpening/BankAccountOpening/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankAccountOpening.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
