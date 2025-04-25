9f5991a3e5f8c025cd0761e1be9fad37a8cfb498
Raghu <raghuraghav929@gmail.com>
2025-04-25 10:16:09 +0530

    Added few more properties in the UserDetails.cs Model file

--- source/repos/BankAccountOpening/BankAccountOpening/Models/UserDetails.cs
+++ source/repos/BankAccountOpening/BankAccountOpening/Models/UserDetails.cs
@@ -4,37 +4,53 @@ namespace BankAccountOpening.Models
 {
     public class UserDetails
     {
+        public int FormNo { get; set; }
         [Required]
         [DataType(DataType.DateTime)]
-        public DateTime dateTime { get; set; }
-        public int FormNo { get; set; }
+        public DateTime dateTime = DateTime.Now;
         [Required]
         public Title title { get; set; }
         [Required]
-        [MaxLength(50, ErrorMessage ="Name can not exceed 50 chars.")]
+        [MaxLength(30, ErrorMessage ="FirstName can not exceed 30 chars.")]
         public string FirstName { get; set; }
+        [MaxLength(30, ErrorMessage = "MiddleName can not exceed 30 chars.")]
         public string MiddleName { get; set; }
         [Required]
-        [MaxLength(50, ErrorMessage = "Name can not exceed 50 chars.")]
+        [MaxLength(30, ErrorMessage = "LastName can not exceed 30 chars.")]
         public string LastName { get; set; }
         [Required]
         public Gender Sex { get; set; }
+        public DateOnly DateOfBirth { get; set; }
         [Required]
-        public int Age { get; set; }
+        public int Age => DateTime.Now.Year - DateOfBirth.Year;
+        [MaxLength(10, ErrorMessage = "Mobile Number can not exceed 10 chars.")]
+        public string StdCode { get; set; }
+        [MaxLength(10, ErrorMessage = "Mobile Number can not exceed 10 chars.")]
+        public string Telephone { get; set; }
         [Required]
         [Phone]
         [Display(Name = "Contact Number")]
+        [MaxLength(10, ErrorMessage = "Mobile Number can not exceed 10 chars.")]
         public string MobileNo {  get; set; }
         [Required]
         [EmailAddress]
+        [MaxLength(255, ErrorMessage = "Email Address can not exceed 255 chars.")]
         [Display(Name = "Email Address")]
         public string EmailId { get; set; }
         [Required]
+        [Display(Name = "State Name")]
+        public string State { get;set; }
+        [Required]
+        [Display(Name = "City Name")]
+        public string City { get; set; }
+        [Required]
         [Display(Name = "Branch Name")]
-        public string Branch { get; set; }
+        public string BranchName { get; set; }
         [Required]
         [Display(Name = "Account Type")]
         public AccountType TypeOfAccount { get; set; }
+        [Required]
+        public string PrefferedLanguage { get; set; }
 
     }
 }
